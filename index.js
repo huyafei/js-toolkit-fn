f75166f0867dd0fa526b811afc625a5886929073
yfhu <879649442@qq.com>
2020-12-17 16:09:02 +0800

    更新

--- index.js
+++
@@ -1,5 +0,0 @@
-
-import fn from './tool/fn'
-
-let tool = Object.assign(fn)
-export default tool;
---
+++ src/utils/index.js
@@ -0,0 +1,10 @@
+export * from "./string.js";
+export * from "./number.js";
+export * from "./arr.js";
+export * from "./date.js";
+export * from "./inspect.js";
+export * from "./dom.js";
+export * from "./http.js";
+export * from "./other.js";
+
+
